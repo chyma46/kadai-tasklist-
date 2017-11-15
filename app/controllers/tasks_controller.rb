d30cee2abc89fee7251859433160d604887d4c48
rnak <rhvx3@mail.umsl.edu>
2017-11-15 11:26:24 +0000

    login kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,13 @@
 class TasksController < ApplicationController
-  include SessionsHelper
   
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks=current_user.tasks.all
   end
   
   def show
-    @task=Task.find(params[:id])
   end
   
   def new
@@ -27,11 +26,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task=Task.find(params[:id])
   end
   
   def update
-    @task=Task.find(params[:id])
     if @task.update(task_params)
       flash[:success]='タスクが更新されました。'
       redirect_to @task
@@ -42,7 +39,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task=Task.find(params[:id])
     @task.destroy
     flash[:success]='タスクが削除されました。'
     redirect_to tasks_url
@@ -52,6 +48,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task=current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
